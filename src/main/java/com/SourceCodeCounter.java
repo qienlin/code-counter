ae1eaf9c272f709ea6d5e6e3aa0c0d97b6cbb70c
qienlin@gmail.com <Daniel@2013>
2013-07-22 23:53:29 +0800

    Make some modification

--- src/main/java/com/SourceCodeCounter.java
+++ src/main/java/com/SourceCodeCounter.java
@@ -27,31 +27,34 @@ public class SourceCodeCounter {
 	 * @throws SAXException
 	 * @throws JAXBException
 	 */
-	public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException,
-			JAXBException {
+	public static void main(String[] args) throws SAXException, IOException,
+			ParserConfigurationException, JAXBException {
 
-		JAXBContext.newInstance(SourceCodeMap.class).createUnmarshaller().unmarshal(new File("conf.xml"));
+		// Get file in current class path
+		JAXBContext.newInstance(SourceCodeMap.class).createUnmarshaller()
+				.unmarshal(ClassLoader.getSystemResourceAsStream("conf.xml"));
 
-//		Document conf = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("conf.xml");
-//		NodeList typeList = conf.getElementsByTagName("type");
-//		for (int i = 0; i < typeList.getLength(); i++) {
-//			NodeList list = typeList.item(i).getChildNodes();
-//			for (int j = 0; j < list.getLength(); j++) {
-//				Node property = list.item(j);
-//				if (property.getNodeType() == Node.ELEMENT_NODE) {
-//					SourceCodeType sourceCodeType = new SourceCodeType();
-//					switch (property.getNodeName()) {
-//					case "extension":
-//						System.out.println(property.getTextContent());
-//						sourceCodeType.setExtension(property.getTextContent());
-//						break;
-//					case "desc":
-//						System.out.println(property.getTextContent());
-//						sourceCodeType.setDesc(property.getTextContent());
-//						break;
-//					}
-//				}
-//			}
-//		}
+		// Document conf =
+		// DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("conf.xml");
+		// NodeList typeList = conf.getElementsByTagName("type");
+		// for (int i = 0; i < typeList.getLength(); i++) {
+		// NodeList list = typeList.item(i).getChildNodes();
+		// for (int j = 0; j < list.getLength(); j++) {
+		// Node property = list.item(j);
+		// if (property.getNodeType() == Node.ELEMENT_NODE) {
+		// SourceCodeType sourceCodeType = new SourceCodeType();
+		// switch (property.getNodeName()) {
+		// case "extension":
+		// System.out.println(property.getTextContent());
+		// sourceCodeType.setExtension(property.getTextContent());
+		// break;
+		// case "desc":
+		// System.out.println(property.getTextContent());
+		// sourceCodeType.setDesc(property.getTextContent());
+		// break;
+		// }
+		// }
+		// }
+		// }
 	}
 }
