a5d4843ac85e16f867f76df6a9c876bac0268d9c
Daniel <qienlin@hengtiansoft.com>
2013-07-23 15:37:12 +0800

    Add CodeCounter

--- src/main/java/com/bootstrap/Bootstrap.java
+++ src/main/java/com/bootstrap/Bootstrap.java
@@ -1,29 +1,19 @@
 package com.bootstrap;
 
-import java.util.Map.Entry;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-
-import com.utils.CodeType;
-import com.utils.Profile;
+import java.io.IOException;
 
 /**
  * @author (Daniel) Jul 22, 2013
  */
 public class Bootstrap {
 
-	public static void main(String[] args) throws JAXBException {
+//	private static String path = "D:/Workspaces/SmartCity/code-counter/src/test/java/com/test/LineTest.java";
 
-		// Get file in current class path
-		Profile profile = (Profile) JAXBContext
-				.newInstance(new Class[] { Profile.class })
-				.createUnmarshaller()
-				.unmarshal(ClassLoader.getSystemResourceAsStream("conf.xml"));
-		for (Entry<String, CodeType> entry : profile.getTypes().entrySet()) {
-			System.out.println(entry.getKey() + "\t"
-					+ entry.getValue().getDesc() + "\t"
-					+ entry.getValue().getSingleLineComment());
-		}
+//		private static String path = "D:/Workspaces/SmartCity/code-counter/src/main/java";
+		
+		private static String path="D:/Workspaces/SmartCity/LYGCG/src";
+
+	public static void main(String[] args) throws IOException {
+		new CodeCounter().read(path);
 	}
 }
